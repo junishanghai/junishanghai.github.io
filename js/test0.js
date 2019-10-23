5e1d3b79590d5b9848fb7f85f177db0e0e22a15e
Jun Ishida <jun.i.shanghai@me.com>
2019-10-24 07:11:01 +0800

    update

--- js/test0.js
+++ js/test0.js
@@ -1,11 +1,13 @@
 export function test0(THREE) {
   /* center */
+  const meshes = []
   let mesh = new THREE.Mesh(
     new THREE.BoxGeometry(10, 10, 10),
     new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
   )
   mesh.position.y = 5
   this.getScene().add(mesh)
+  meshes.push(mesh)
 
   /* left */
   mesh = new THREE.Mesh(
@@ -15,6 +17,7 @@ export function test0(THREE) {
   mesh.position.x = -15
   mesh.position.y = 5
   this.getScene().add(mesh)
+  meshes.push(mesh)
 
   /* right */
   mesh = new THREE.Mesh(
@@ -24,6 +27,7 @@ export function test0(THREE) {
   mesh.position.x = 15
   mesh.position.y = 5
   this.getScene().add(mesh)
+  meshes.push(mesh)
 
   /* back */
   mesh = new THREE.Mesh(
@@ -33,10 +37,12 @@ export function test0(THREE) {
   mesh.position.y = 5
   mesh.position.z = 25
   this.getScene().add(mesh)
+  meshes.push(mesh)
 
   // test element add to rayReceiveObjects
   // this.rayReceiveObjects = this.rayReceiveObjects.concat(
   //  this.getScene().children
   // )
   this.rayReceiveObjects.push(mesh)
+  return meshes
 }
